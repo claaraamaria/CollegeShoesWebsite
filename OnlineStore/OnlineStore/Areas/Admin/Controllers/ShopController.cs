d32a7ae2c20bca59ed07447b940ea2ab8098234c
claaraa <clrstancu@gmail.com>
2020-01-13 00:11:37 +0200

    The post-back method for adding the product

--- OnlineStore/OnlineStore/Areas/Admin/Controllers/ShopController.cs
+++ OnlineStore/OnlineStore/Areas/Admin/Controllers/ShopController.cs
@@ -339,6 +339,62 @@ namespace OnlineStore.Areas.Admin.Controllers
             return View(model);
         }
 
+        //POST: Admin/Shop/EditProduct/id
+        [HttpPost]
+        public ActionResult EditProduct(ProductVM model, HttpPostedFileBase file)
+        {
+            //Get product id
+            int id = model.Id;
+            //populate categories select list and gallery images
+
+            using (Db db = new Db())
+            {
+                model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+            }
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs"))
+                                                .Select(fn => Path.GetFileName(fn));
+
+            //check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            //make sure product name is unique
+            using (Db db = new Db())
+            {
+                if(db.Products.Where(x => x.Id != id).Any(x => x.Name == model.Name))
+                {
+                    ModelState.AddModelError("", "That product name is taken!");
+                    return View(model);
+                }
+            }
+
+            //update product
+            using (Db db = new Db())
+            {
+                ProductDTO dto = db.Products.Find(id);
+
+                dto.Name = model.Name;
+                dto.Slug = model.Name.Replace(" ", "-").ToLower();
+                dto.Price = model.Price;
+                dto.CategoryId = model.CategoryId;
+                dto.ImageName = model.ImageName;
+
+                CategoryDTO catDTO = db.Categories.FirstOrDefault(x => x.Id == model.Id);
+                dto.CategoryName = catDTO.Name;
+
+                db.SaveChanges();
+            }
+            //set TempData message
+            TempData["SM"] = "You have edited the product!";
+            #region Image upload
+
+            #endregion
+
+            //redirect
+            return View();
+        }
 
         /*
          public ActionResult DeleteCategory()
